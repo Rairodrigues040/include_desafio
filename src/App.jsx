c158c76c58479ca1d3bb27341702d7fe3381a7bd
José Alisson Paz Lopes <alissonlino31@gmail.com>
2025-06-05 21:44:11 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -20,3 +20,31 @@ const App = () => {
 };
 
 export default App;
+
+/* 
+import React from 'react';
+import Cards from './components/cards/Cards'
+import PetCards from './components/cards/PetCard'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+      <main>
+        <p>Esse é o conteúdo principal.</p>
+        <Cards/>
+        <PetCards/>
+      </main>
+      <footer>
+        <p>&copy; 2025 Meu App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
+
+*/
