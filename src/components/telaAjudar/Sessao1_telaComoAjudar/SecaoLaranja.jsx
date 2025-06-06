c2f723e3618dc9f9cf34bbd4ebc2926790643b8f
José Alisson Paz Lopes <alissonlino31@gmail.com>
2025-06-06 13:59:25 -0300

    up

---
+++ src/components/telaAjudar/Sessao1_telaComoAjudar/SecaoLaranja.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SecaoLaranja.css';
+
+function SecaoLaranja() {
+  return (
+    <section className="secao-laranja">
+      <h1>Sua Ajuda Transforma Vidas</h1>
+      <p>
+        Cada gesto de amor conta. Descubra como você pode fazer a diferença
+        <br />
+        na vida de um animal carente e se tornar parte desta rede de
+        <br />
+        esperança e transformação.
+      </p>
+    </section>
+  );
+}
+
+export default SecaoLaranja;
